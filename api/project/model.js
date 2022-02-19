786100338193c2e851dc117eaa48ee7ca7b3967f
Ogden-R <rebeccaogden97@gmail.com>
2022-02-18 18:49:45 -0600

    all tests passing

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,42 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+async function getAll() {
+    const allProjects = await db('projects')
+
+    const projects = allProjects.map((project) => {
+        const formattedProject = {
+            project_id: project.project_id,
+            project_name: project.project_name,
+            project_description: project.project_description,
+            project_completed: project.project_completed === 0 ? false : true,
+        }
+        return formattedProject
+    })
+    return projects
+}
+
+async function getById(project_id) {
+    const singleProject = await db('projects')
+        .where('project_id', project_id)
+        .first()
+
+    const projectFormatted = {
+        project_id: singleProject.project_id,
+        project_name: singleProject.project_name,
+        project_description: singleProject.project_description,
+        project_completed: singleProject.project_completed === 0 ? false : true,
+    }
+    return projectFormatted
+}
+
+async function addProject(project) {
+    const [project_id] = await db('projects').insert(project)
+    return getById(project_id)
+}
+
+    module.exports = {
+        getAll,
+        addProject,
+        getById,
+    };
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,29 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function fetchAllTasks() {
+    const rows = await db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select('t.*', 'p.project_name', 'p.project_description')
+    return rows.map((item) => {
+        return {
+            ...item,
+            task_completed: item.task_completed ? true : false,
+        }
+    })
+}
+
+async function findTaskById(id) {
+    const row = await db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select('t.*', 'p.project_name', 'p.project_description')
+        .where('task_id', id)
+        .first()
+    return { ...row, task_completed: row.task_completed ? true : false }
+}
+
+async function insert(task) {
+    const [id] = await db('tasks').insert(task)
+    return findTaskById(id)
+}
+module.exports = { fetchAllTasks, findTaskById, insert }
